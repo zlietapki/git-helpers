d16ef6a73c7da5f8bef5aaac18ad840ec13a3f67
Andrey Martemyanov <andrey.zloy@gmail.com>
2020-09-28 17:12:17 +0300

    +columns

--- githelpers/__init__.py
+++ githelpers/__init__.py
@@ -1,5 +1,25 @@
+import re
 import subprocess
 
 
 def run(cmd):
     return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').rstrip()
+
+
+def get_len(colored_str):
+    escape_sequences_sub = re.compile(r"""
+        \x1b     # literal ESC
+        \[       # literal [
+        [;\d]*   # zero or more digits or semicolons
+        [A-Za-z] # a letter
+        """, re.VERBOSE).sub
+    uncolored = escape_sequences_sub("", colored_str)
+    return len(uncolored)
+
+
+def as_col(text, need_size):
+    real_len = get_len(text)
+    spaces = need_size - real_len
+    if spaces > 0:
+        text += " " * spaces
+    return text
